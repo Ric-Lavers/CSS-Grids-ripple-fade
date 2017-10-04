6ae15170acd7c883a962652c15ed7f80cf0e4f9e
Ric-Lavers <ric_lavers@outlook.com>
2017-10-05 08:54:10 +1100

    working with right edge bug

--- main.js
+++ main.js
@@ -1,52 +1,81 @@
-
-var fade = 60;
-var rows = 8;
-var cols = 8;
+function test(){
+  test=[1,2,4,5,6];
+  for(i=0;i<7;i++){
+    console.log(i);
+    // console.log("test.indexOf(i)"+test.indexOf(i));
+    if(test.indexOf(i) ===-1){console.log("Not there");}
+    else if (test.indexOf(i) !=-1){console.log("yes its there")};
+  };
+};
+test()
+function playin(){
+  $(document).ready(function() {
+    $('.box').click(function(){
+      $(".grid").toggleClass('grid2');
+    });
+  });
+};
+var stagger = 60;
+var fade = 40;
+var rows = 16;
+var cols = 16;
 
 $(document).ready(function() {
-  $('.test').click(function(e){
+  $('.box').click(function(e){
     //find the id of the click object
     //**note** the pattern of divs is important!! a-z = rows & nums = cols
     var id = e.currentTarget.id;
-    $(this).fadeTo(600,0.01);
+    $(this).toggleClass('clear');
     revealAll(id);
   });
 });
 
 function revealAll(id){
   //useful for setting the revealed divs
-  row = ['a', 'b', 'c', 'd', 'e',
-         'f', 'g', 'h', 'i', 'j',
-         'k', 'l', 'm', 'n', 'o',
-         'p', 'q', 'r', 's', 't',
-         'u', 'v', 'w', 'x', 'y', 'z' ];
+  row = ['a', 'b', 'c', 'd', 'e','f', 'g', 'h', 'i', 'j',
+         'k', 'l', 'm', 'n', 'o','p', 'q', 'r', 's', 't',
+         'u', 'v', 'w', 'x', 'y', 'z', 'A' ,'B' ,'C' ,'D',
+         'E' ,'F' ,'G' ,'H' ,'I' ,'J' ,'K' ,'L' ,'M' ,'N',
+         'O' ,'P' ,'Q' ,'R' ,'S' ,'T' ,'U' ,'V' ,'W' ,'X','Y' ,'Z'];
   var r = row.indexOf(id[0]);
   var col = parseInt(id[1]);
 
   var x=1;
+  var used=[]
   loop(x);
 
   function loop(x){
-    if (x<6){
+    if (x<(rows)){
         console.log("x "+x);
         setTimeout(function(){
           for (i = -1*x; i <=1*x; i++){
             for(j = -1*x; j <=1*x; j++){
-              if (row[r+i]+(j+col) != id)
-              {$("#"+row[r+i]+(j+col)).fadeTo(600,0.01);
-              };
+              if (row[r+i]+(j+col) != id && used.indexOf(row[r+i]+(j+col))===-1 )
+              { $("#"+row[r+i]+(j+col)).toggleClass('clear');
+                // used.push([i,j]);
+                used.push(row[r+i]+(j+col))
+
+              // console.log("#"+row[r+i]+(j+col));
             };
-          };
+            };//j
+          };//i
+          // console.log("used.indexOf([i,j])"+used.indexOf([i,j]));
+          // console.log(used);
+          // if (x===2){return}
           x++;
           loop(x);
-      },400);
-
+      },stagger);
     }else{return};
   };//loop
 }
 
 
 
+ // border= 33333  (-2,-2)(-1,-2)(0,-2)(1,-2)(2,-2)        i= -1*2;  i <=2; i++
+ //         32223  (-2,-1)(     )(    )(    )(2,-1)
+ //         32123  (-2, 0)(     )( ++ )(    )(2, 0)
+ //         32223  (-2, 1)(     )(    )(    )(2, 1)
+ //         33333  (-2, 2)( 1, 2)(0, 2)(1, 2)(2, 2)
 
 
 //
